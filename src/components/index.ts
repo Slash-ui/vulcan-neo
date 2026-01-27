b63f4333ac208a7d4e8ceb607805300f59f5970a
Amin Shariati <amin.shariati@gmail.com>
2026-01-27 22:31:41 +0800

    feat: Add the new component to the component library

--- src/components/index.ts
+++ src/components/index.ts
@@ -23,3 +23,96 @@ export type {
   CardBodyProps,
   CardFooterProps,
 } from './Card';
+
+export { FeaturedIcon } from './FeaturedIcon';
+export type {
+  FeaturedIconProps,
+  FeaturedIconVariant,
+  FeaturedIconSize,
+  FeaturedIconElevation,
+  FeaturedIconColor,
+  FeaturedIconShape,
+} from './FeaturedIcon';
+
+export { IconButton } from './IconButton';
+export type {
+  IconButtonProps,
+  IconButtonVariant,
+  IconButtonSize,
+  IconButtonElevation,
+  IconButtonShape,
+} from './IconButton';
+
+export { Badge } from './Badge';
+export type { BadgeProps, BadgeVariant, BadgeSize, BadgeColor } from './Badge';
+
+export { Avatar } from './Avatar';
+export type { AvatarProps, AvatarVariant, AvatarSize, AvatarStatus } from './Avatar';
+
+export { SocialButton } from './SocialButton';
+export type {
+  SocialButtonProps,
+  SocialButtonVariant,
+  SocialButtonSize,
+  SocialButtonElevation,
+} from './SocialButton';
+
+export { AppStoreButton } from './AppStoreButton';
+export type {
+  AppStoreButtonProps,
+  AppStoreButtonVariant,
+  AppStoreButtonSize,
+  AppStoreButtonElevation,
+  AppStoreButtonStore,
+} from './AppStoreButton';
+
+export { ButtonGroup, ButtonGroupItem } from './ButtonGroup';
+export type {
+  ButtonGroupProps,
+  ButtonGroupItemProps,
+  ButtonGroupVariant,
+  ButtonGroupSize,
+  ButtonGroupElevation,
+  ButtonGroupOrientation,
+} from './ButtonGroup';
+
+export { Textarea } from './Textarea';
+export type { TextareaProps, TextareaSize } from './Textarea';
+
+export { Checkbox } from './Checkbox';
+export type { CheckboxProps, CheckboxSize } from './Checkbox';
+
+export { Radio } from './Radio';
+export type { RadioProps, RadioSize } from './Radio';
+
+export { RadioGroup, RadioGroupItem } from './RadioGroup';
+export type {
+  RadioGroupProps,
+  RadioGroupItemProps,
+  RadioGroupSize,
+  RadioGroupOrientation,
+} from './RadioGroup';
+
+export { VerificationCodeInput } from './VerificationCodeInput';
+export type { VerificationCodeInputProps, VerificationCodeInputSize } from './VerificationCodeInput';
+
+export { Slider } from './Slider';
+export type { SliderProps, SliderSize } from './Slider';
+
+export { Select } from './Select';
+export type { SelectProps, SelectOption, SelectSize } from './Select';
+
+export { Dropdown } from './Dropdown';
+export type { DropdownProps, DropdownItem, DropdownPlacement } from './Dropdown';
+
+export { Tag } from './Tag';
+export type { TagProps, TagVariant, TagSize, TagColor } from './Tag';
+
+export { BadgeGroup } from './BadgeGroup';
+export type { BadgeGroupProps, BadgeGroupSize } from './BadgeGroup';
+
+export { Tooltip } from './Tooltip';
+export type { TooltipProps, TooltipPlacement } from './Tooltip';
+
+export { RatingStars } from './RatingStars';
+export type { RatingStarsProps, RatingStarsSize } from './RatingStars';
