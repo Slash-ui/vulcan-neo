be64e997b0bc0b0988202690743dede4d29bcfd5
Amin Shariati <amin.shariati@gmail.com>
2026-01-28 08:44:41 +0800

    feat: Update componet library

--- src/components/index.ts
+++ src/components/index.ts
@@ -116,3 +116,151 @@ export type { TooltipProps, TooltipPlacement } from './Tooltip';
 
 export { RatingStars } from './RatingStars';
 export type { RatingStarsProps, RatingStarsSize } from './RatingStars';
+
+// Navigation Components
+export { Breadcrumbs, BreadcrumbItem } from './Breadcrumbs';
+export type { BreadcrumbsProps, BreadcrumbItemProps } from './Breadcrumbs';
+
+export { Tabs, TabList, Tab, TabPanel } from './Tabs';
+export type { TabsProps, TabListProps, TabProps, TabPanelProps, TabsVariant, TabsSize } from './Tabs';
+
+export { Pagination } from './Pagination';
+export type { PaginationProps, PaginationSize } from './Pagination';
+
+export { SidebarNavigation, SidebarNavGroup, SidebarNavItem } from './SidebarNavigation';
+export type {
+  SidebarNavigationProps,
+  SidebarNavGroupProps,
+  SidebarNavItemProps,
+  SidebarSize,
+} from './SidebarNavigation';
+
+export { HeaderNavigation, HeaderNavItem, HeaderNavGroup } from './HeaderNavigation';
+export type { HeaderNavigationProps, HeaderNavItemProps, HeaderNavGroupProps, HeaderSize } from './HeaderNavigation';
+
+// Overlay Components
+export { Modal } from './Modal';
+export type { ModalProps, ModalSize } from './Modal';
+
+export { SlideoutMenu } from './SlideoutMenu';
+export type { SlideoutMenuProps, SlideoutPosition, SlideoutSize } from './SlideoutMenu';
+
+export { CommandMenu } from './CommandMenu';
+export type { CommandMenuProps, CommandItem } from './CommandMenu';
+
+// Feedback Components
+export { Alert } from './Alert';
+export type { AlertProps, AlertVariant } from './Alert';
+
+export { Notification } from './Notification';
+export type { NotificationProps, NotificationVariant, NotificationPosition } from './Notification';
+
+export { LoadingIndicator, Skeleton } from './LoadingIndicator';
+export type { LoadingIndicatorProps, LoadingSize, LoadingVariant, SkeletonProps } from './LoadingIndicator';
+
+export { EmptyState } from './EmptyState';
+export type { EmptyStateProps, EmptyStateSize } from './EmptyState';
+
+export { ProgressSteps } from './ProgressSteps';
+export type { ProgressStepsProps, ProgressStepsSize, ProgressStepsOrientation, Step } from './ProgressSteps';
+
+// Data Display Components
+export { Table } from './Table';
+export type { TableProps, TableVariant, TableSize, Column } from './Table';
+
+export { Metrics, MetricCard } from './Metrics';
+export type { MetricsProps, MetricCardProps, MetricItem, MetricsSize, MetricsTrend } from './Metrics';
+
+export { ActivityFeed } from './ActivityFeed';
+export type { ActivityFeedProps, ActivityFeedSize, ActivityItem } from './ActivityFeed';
+
+export { CodeSnippet } from './CodeSnippet';
+export type { CodeSnippetProps, CodeSnippetSize } from './CodeSnippet';
+
+// Input Components
+export { Calendar } from './Calendar';
+export type { CalendarProps } from './Calendar';
+
+export { DatePicker } from './DatePicker';
+export type { DatePickerProps, DatePickerSize } from './DatePicker';
+
+export { FileUploader } from './FileUploader';
+export type { FileUploaderProps, UploadedFile } from './FileUploader';
+
+// Interactive Components
+export { Carousel } from './Carousel';
+export type { CarouselProps, CarouselSize } from './Carousel';
+
+export { InlineCTA } from './InlineCTA';
+export type { InlineCTAProps, InlineCTAVariant } from './InlineCTA';
+
+export { Messaging, MessageBubble, MessageInput } from './Messaging';
+export type {
+  MessagingProps,
+  MessageBubbleProps,
+  MessageInputProps,
+  Message,
+  MessagePosition,
+} from './Messaging';
+
+// Chart Components
+export { LineChart } from './LineChart';
+export type { LineChartProps, LineChartDataPoint, LineChartSeries } from './LineChart';
+
+export { BarChart } from './BarChart';
+export type { BarChartProps, BarChartDataPoint } from './BarChart';
+
+export { PieChart } from './PieChart';
+export type { PieChartProps, PieChartDataPoint } from './PieChart';
+
+// Marketing Components
+export { MarketingHeader } from './MarketingHeader';
+export type { MarketingHeaderProps, MarketingNavItem } from './MarketingHeader';
+
+export { HeroSection } from './HeroSection';
+export type { HeroSectionProps, HeroSectionVariant, HeroSectionSize } from './HeroSection';
+
+export { FeaturesSection } from './FeaturesSection';
+export type { FeaturesSectionProps, FeaturesSectionLayout, FeatureItem } from './FeaturesSection';
+
+export { PricingSection } from './PricingSection';
+export type { PricingSectionProps, PricingPlan, PricingFeature } from './PricingSection';
+
+export { CTASection } from './CTASection';
+export type { CTASectionProps, CTASectionVariant } from './CTASection';
+
+export { StatsSection } from './StatsSection';
+export type { StatsSectionProps, StatItem } from './StatsSection';
+
+export { NewsletterSection } from './NewsletterSection';
+export type { NewsletterSectionProps, NewsletterSectionVariant } from './NewsletterSection';
+
+export { TestimonialSection } from './TestimonialSection';
+export type { TestimonialSectionProps, Testimonial } from './TestimonialSection';
+
+export { LogoCloudSection } from './LogoCloudSection';
+export type { LogoCloudSectionProps, LogoItem } from './LogoCloudSection';
+
+export { BlogSection } from './BlogSection';
+export type { BlogSectionProps, BlogPost } from './BlogSection';
+
+export { ContactSection } from './ContactSection';
+export type { ContactSectionProps, ContactInfo } from './ContactSection';
+
+export { TeamSection } from './TeamSection';
+export type { TeamSectionProps, TeamMember } from './TeamSection';
+
+export { CareersSection } from './CareersSection';
+export type { CareersSectionProps, JobPosition } from './CareersSection';
+
+export { FAQSection } from './FAQSection';
+export type { FAQSectionProps, FAQItem } from './FAQSection';
+
+export { ContentSection } from './ContentSection';
+export type { ContentSectionProps } from './ContentSection';
+
+export { Footer } from './Footer';
+export type { FooterProps, FooterLink, FooterLinkGroup, FooterSocialLink } from './Footer';
+
+export { Banner } from './Banner';
+export type { BannerProps, BannerVariant, BannerPosition } from './Banner';
